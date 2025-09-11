78eeffcdb304e3c99fadeb2f91b51808329621a2
Armanduqui <armando_2408@hotmail.com>
2025-09-11 17:14:18 -0600

    Se vuelven a habilitar los puntos, descomente las lineas que cancelan los puntos, se prueba y funcionan los puntos, tambien realice cambio de version manualmente en el MainActivity

--- app/src/androidTest/java/mx/greenmouse/kaliope/ApplicationTest.java
+++ app/src/androidTest/java/mx/greenmouse/kaliope/ApplicationTest.java
@@ -1,13 +1,18 @@
 package mx.greenmouse.kaliope;
 
 import android.app.Application;
-import android.test.ApplicationTestCase;
+/* import android.test.ApplicationTestCase; */
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
+/*
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
 }
+*/
+
+/* No se por que pero al compilar en android studio mas reciente da errror en la clas al importar android.test ademas dice  */
+/* Que nunca se usa la clase, la voy a commentar y ya compilo y se iso el apk esta raro pero asi es android studio */
